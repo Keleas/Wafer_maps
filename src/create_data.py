053726105eea49a37841a87b4a66b8d6f94b21d0
Keleas <grishin.na@phustech.edu>
2019-08-11 23:14:56 +0300

    upd code

--- src/create_data.py
+++ src/create_data.py
@@ -2,6 +2,25 @@ import numpy as np
 import pandas as pd
 import os
 
+import matplotlib.pyplot as plt
+import cv2
+import math
+import random
+
+from multiprocessing import cpu_count
+from joblib import Parallel, delayed
+cpu_count = cpu_count()
+
+IMAGE_DIMS = (92, 92, 1)
+wafer_map = pd.read_pickle('test_wafer_map.pkl')
+test = cv2.resize(wafer_map.waferMap, dsize=(IMAGE_DIMS[0],IMAGE_DIMS[1]),
+                  interpolation=cv2.INTER_NEAREST)
+
+## 2 - паттерн
+## 1 - фон
+## 0 - область, где нет ничего
+test[test == 2] = 1
+
 
 def read_data(path):
     df_all_standart = pd.read_pickle(os.path.join(os.getcwd(), path))
